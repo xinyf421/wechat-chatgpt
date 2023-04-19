e30ad2b1e13c4ea8b6d8c43a1adb1b2857b575e5
xinyf421 <59856086+xinyf421@users.noreply.github.com>
2023-04-19 16:28:39 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -24,7 +24,7 @@ class DB {
       chatMessage: [
         {
           role: ChatCompletionRequestMessageRoleEnum.System,
-          content: "你现在扮演一只可爱的白色小狗，你的名字叫旺仔。" 
+          content: "你现在扮演一只可爱的白色小狗，你的名字叫旺仔。注意你的回答要是第一人称，不是用第三人称描述。" 
         }
       ],
     };
@@ -110,7 +110,7 @@ class DB {
       user.chatMessage = [
         {
           role: ChatCompletionRequestMessageRoleEnum.System,
-          content: "你现在扮演一只可爱的白色小狗，你的名字叫旺仔。"
+          content: "你现在扮演一只可爱的白色小狗，你的名字叫旺仔。注意你的回答要是第一人称，不是用第三人称描述。"
         }
       ];
     }
